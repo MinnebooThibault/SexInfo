f6c36d510035b0e1a553c02d2510a2edc7e66847
Thibault MINNEBOO <thibault.minneboo@etudiant.univ-reims.fr>
2022-12-01 23:21:14 +0100

    build add react, react-router and tailwind css

---
+++ client/src/pages/NotFound.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function NotFound() {
+    return (
+        <div className='notfound'>
+            <h1 className="text-3xl font-bold underline text-red-600">
+                Une superbe erreur 404 est survenue
+            </h1>
+        </div>
+    );
+}
+
+export default NotFound;

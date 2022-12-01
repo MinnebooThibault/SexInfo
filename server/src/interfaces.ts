074d339ba6f804e7d81d3c16c3aa2c2a21609c1c
ledo0024 <simon.ledoux@etudiant.univ-reims.fr>
2022-12-01 23:57:39 +0100

    Implémentation de la base de données

---
+++ server/src/interfaces.ts
@@ -0,0 +1,45 @@
+export interface IQuestion {
+    id: number;
+    label: string;
+    niveauDifficulte: number;
+    reponse1: string;
+    reponse2: string;
+    reponse3: string;
+    reponse4: string;
+    explication: string;
+};
+
+export interface IHistorique {
+    id: number;
+    date: string;
+    position: number;
+};
+
+export interface IObjet {
+    id: number;
+    type: number;
+    label: string;
+    description: string;
+    prix: number;
+};
+
+export interface ISkin {
+    id: number;
+    label: string;
+};
+
+export interface IAchievement {
+    id: number;
+    label: string;
+    description: string;
+};
+
+export interface IUser {
+    id: number;
+    xp: number;
+    pieces: number;
+    skinCourant: ISkin;
+    skinsPossedes: ISkin[];
+    objetsPossedes: IObjet[];
+    achievement: IAchievement[];
+};
